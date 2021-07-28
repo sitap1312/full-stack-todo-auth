50a39c62e9397ea7cb9b8c71ffd991a668fe0307
corey gibson <cgibsonmm@gmail.com>
2021-07-28 15:17:00 -0400

    RED: added restrict

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import usersRoutes from "./user.js";
+import todoRoutes from "./todo.js";
 
 const router = Router();
 
 router.get("/", (req, res) => res.send("This is api root"));
 
 router.use("/", usersRoutes);
-// router.use("/", todosRoutes)
+router.use("/", todoRoutes);
 
 export default router;
---
+++ routes/todo.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import restrict from "../helpers/restrict.js";
+
+const router = Router();
+// get all todos
+router.get("/todos", getAllTodos);
+// get id todo
+router.get("/todos/:id", getTodo);
+// post createtodo
+router.post("/todos", restrict, createTodo);
+// put updateTodo
+router.put("/todos/:id", restrict, updateTodo);
+// delete todo
+router.delete("/todos/:id", restrict, deleteTodo);
+
+export default router;
