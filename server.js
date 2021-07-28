69b8bf4f745f2bce34ea4dd2ae1b2d5bf22ee2a4
corey gibson <cgibsonmm@gmail.com>
2021-07-28 11:05:09 -0400

    added server.js and scripts

--- server.js
+++ server.js
@@ -2,3 +2,18 @@
 // const express = require("express");
 // type: module
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+const PORT = process.env.PORT || 4567;
+
+// gives access to req.body
+app.use(express.json());
+app.use(cors());
+// change after deploy and we know everything is working 'tiny'
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => res.send("<h1>Hello, world</h1>"));
+
+app.listen(PORT, console.log(`Connected to port: ${PORT}`));
