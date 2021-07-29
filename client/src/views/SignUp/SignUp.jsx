c5fd7ccca8fd64040fb110417738d804c4ebba7a
corey gibson <cgibsonmm@gmail.com>
2021-07-29 14:03:18 -0400

    Green: added sign up

--- client/src/views/SignUp/SignUp.jsx
+++ client/src/views/SignUp/SignUp.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
+import { signUp } from "../../services/users";
 
 export default function SignUp() {
   const [input, setInput] = useState({ username: "", email: "", password: "" });
 
-  const handleSubmit = () => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const user = await signUp(input);
+    console.log(user);
+  };
 
   const handleInput = (e) => {
     const { id, value } = e.target;
@@ -17,26 +22,32 @@ export default function SignUp() {
       SignUp
       <form onSubmit={handleSubmit}>
         <label>Username</label>
+        <br />
         <input
           id="username"
           type="text"
           value={input.username}
           onChange={handleInput}
         />
+        <br />
         <label>Email</label>
+        <br />
         <input
           id="email"
           type="email"
           value={input.email}
           onChange={handleInput}
         />
+        <br />
         <label>Password</label>
+        <br />
         <input
           id="password"
           type="password"
           value={input.password}
           onChange={handleInput}
         />
+        <br />
         <button>Sign Up</button>
       </form>
     </div>
