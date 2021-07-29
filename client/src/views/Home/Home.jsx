389d24f79d7f48586bf6416f9bdf99e727e73e18
corey gibson <cgibsonmm@gmail.com>
2021-07-29 12:23:51 -0400

    added api config getall and view

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import Layout from "../../components/Layout/Layout";
+import { getAllTodos } from "../../services/todos.js";
 
 export default function Home() {
-  return <Layout>Home</Layout>;
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      let data = await getAllTodos();
+      setTodos(data);
+    };
+    fetchTodos();
+  }, []);
+
+  return (
+    <Layout>
+      {todos.map((todo) => (
+        <div>{todo.name}</div>
+      ))}
+    </Layout>
+  );
 }
